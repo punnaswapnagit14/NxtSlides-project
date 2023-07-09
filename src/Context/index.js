6591a58d6b4e65af394a7728bbe58d5cc0728f31
punnaswapnagit14 <punnaswapnagit14@gmail.com>
2023-07-09 10:57:42 +0000

    new commit

---
+++ src/Context/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SlideContext = React.createContext({
+  initialList: [],
+  activeIndex: 0,
+  changeActiveTab: () => {},
+  addNewItem: () => {},
+  changeHeading: () => {},
+  changeDescription: () => {},
+})
+export default SlideContext
